3100383a7338981b785ced95dd8bff788da8b7ca
olegovich22 <o.yakymenko22@gmail.com>
2020-12-22 15:06:30 +0200

    add secret dev section with temperature and humidity

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ app = Flask(__name__)
 
 import csv
 
+_temperature = 25
+_humidity = 70
+
 @app.route('/')
 def my_home():
     return render_template('index.html')
@@ -10,7 +13,12 @@ def my_home():
 
 @app.route('/<string:page_name>')
 def works(page_name):
-    return render_template(page_name+'.html')
+    return render_template(page_name +'.html', temperature=_temperature, humidity=_humidity)
+
+# @app.route('/dd')
+# def works():
+#     print('some trst msg')
+#     return render_template('develop.html', temperature='909')
 
 
 def write_to_file(data):
@@ -43,3 +51,37 @@ def submit_form():
     else:
         return "smth went wrong"
 
+
+@app.route('/submit_temp', methods=['POST', 'GET'])
+def submit_temp():
+    global _humidity
+    global _temperature
+
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            _temperature = data['temperature']
+            _humidity = data['humidity']
+
+            return redirect('develop')
+        except:
+            return 'did not save to database'
+    else:
+        return "smth went wrong"
+
+#http://127.0.0.1:5000/submit_temp_get?temperature=10&humidity=50
+@app.route('/submit_temp_get', methods=['POST', 'GET'])
+def submit_temp_get():
+    global _humidity
+    global _temperature
+
+    if request.method == 'GET':
+        try:
+            data = request.args
+            _temperature = data['temperature']
+            _humidity = data['humidity']
+            return "OK"
+        except:
+            return 'did not save to database'
+    else:
+        return "smth went wrong"
